7364e90956f80df58952c99eec1164512fc5edbb
rodrigopberto <56567625+rodrigopberto@users.noreply.github.com>
2019-11-14 12:15:42 -0300

    Update example2.py

--- example2.py
+++ example2.py
@@ -22,7 +22,7 @@ default_args = {
     # 'end_date': datetime(2016, 1, 1),
 }
 
-dag = DAG("tutorial2", default_args=default_args, schedule_interval=timedelta(1))
+dag = DAG("tutorial2", default_args=default_args, schedule_interval=timedelta(1),access_control={'role1':['can_dag_read']})
 
 # t1, t2 and t3 are examples of tasks created by instantiating operators
 t1 = BashOperator(task_id="print_date", bash_command="date", dag=dag)
